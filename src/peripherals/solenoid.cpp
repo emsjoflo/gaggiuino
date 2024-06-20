a472f116caa45f63d46e98834eb566775ee2dd15
Andrew Florian <andyhflo@gmail.com>
2024-06-20 08:02:01 -0400

    enable solenoid in manual mode, improve calibration

--- src/peripherals/solenoid.cpp
+++ src/peripherals/solenoid.cpp
@@ -1,13 +1,15 @@
 /* 09:32 15/03/2023 - change triggering comment */
 #include "Solenoid.h"
+#include "pump.h"
 #include "pindef.h"
 #include "utils.h"
 #include "peripherals.h"
 
 void setSolenoidToRawValue(const uint8_t val) {
-  if (millis() % 1000 > val) {
-    setSteamValveRelayOn(); //H2
-  } else{
+ setPumpToRawValue(100);
+  if (millis() % 1000 > val*10.f) {
     setSteamValveRelayOff(); //H2
+  } else{
+    setSteamValveRelayOn(); //H2
   }
 }
